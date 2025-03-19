3ad338d7a0e45738464febbf10ccd3c42949bfde
olstra <seriously251@gmail.com>
2025-03-19 18:56:25 +0100

    Fixup with previous commit

--- src/file_handling/text_extractor.py
+++ src/file_handling/text_extractor.py
@@ -1,5 +1,10 @@
 from pypdf import PdfReader
 
+from src.nlp.text_cleaner import clean_up_line_breaks
+
 
 def extract_text_from_pdf(path: str) -> str:
-    return ''.join(page.extract_text() for page in PdfReader(path).pages)
+    text = ''.join(page.extract_text() for page in PdfReader(path).pages)
+    text = clean_up_line_breaks(text.split('\n'))
+    text = '\n'.join(sentence for sentence in text)
+    return text
